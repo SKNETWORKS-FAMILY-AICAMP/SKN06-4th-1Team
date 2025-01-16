da44bc7447ba094e28dcc51ff83fa61fef648590
Realoil <chungyuj33@gmail.com>
2025-01-16 16:41:55 +0900

    account/ views.py 수정

--- account/views.py
+++ account/views.py
@@ -7,62 +7,67 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
 from .models import User
-from .forms import 생성폼, 변경폼
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 # 용어들(변경필요)
-    # 사용자 생성 : 생성폼
-    # 사용자 정보 변경 : 변경폼
-    # 첫 화면 : HOME
-
+# 첫 화면 : HOME
 
 
 # 사용자 가입
 def create(request):
     if request.method == "GET":
-        return render(request, 'account/create.html', {'form':생성폼()})
+        return render(
+            request, "account/create.html", {"form": CustomUserCreationForm()}
+        )
     elif request.method == "POST":
-        form = 생성폼(request.POST, request.FILES)
+        form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(reverse('HOME'))
+            return redirect(reverse("HOME"))
         else:
-            return render(request, 'account/create.html', {'form':form})
-
+            return render(request, "account/create.html", {"form": form})
 
 
 # 사용자 로그인
 @login_required
 def login(request):
     if request.method == "GET":
-        return render(request, 'account/login.html', {'form':AuthenticationForm()})
+        return render(request, "account/login.html", {"form": AuthenticationForm()})
     elif request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            if request.GET.get('next'):
-                return redirect(request.GET.get('next'))
+            if request.GET.get("next"):
+                return redirect(request.GET.get("next"))
             else:
-                return redirect(reverse('HOME'))
+                return redirect(reverse("HOME"))
         else:
-            return render(request, 'account/login.html', {'form':AuthenticationForm(), "error_msg": "아이디나 비밀번호를 다시 확인해주세요."})
+            return render(
+                request,
+                "account/login.html",
+                {
+                    "form": AuthenticationForm(),
+                    "error_msg": "아이디나 비밀번호를 다시 확인해주세요.",
+                },
+            )
 
 
 # 사용자 로그아웃
 @login_required
 def logout(request):
     logout(request)
-    return redirect(reverse('HOME'))
+    return redirect(reverse("HOME"))
 
 
 # 사용자 정보 조회
 @login_required
-def detail(request):    
+def detail(request):
     user = User.objects.get(pk=request.user.pk)
-    return render(request, 'account/detail.html', {'user':user})
+    return render(request, "account/detail.html", {"user": user})
 
 
 # 사용자 비밀번호 변경
@@ -71,7 +76,7 @@ def pwd_change(request):
     http_method = request.method
     if http_method == "GET":
         form = PasswordChangeForm(user=request.user)
-        return render(request, 'account/pwd_change.html', {'form':form})
+        return render(request, "account/pwd_change.html", {"form": form})
     elif http_method == "POST":
         form = PasswordChangeForm(user=request.user, data=request.POST)
         if form.is_valid():
@@ -79,7 +84,11 @@ def pwd_change(request):
             update_session_auth_hash(request, user)
             return redirect(reverse("account:detail"))
         else:
-            return render(request, 'account/pwd_change.html', {'form':form, "error_msg":"유효하지 않은 비밀번호입니다."})
+            return render(
+                request,
+                "account/pwd_change.html",
+                {"form": form, "error_msg": "유효하지 않은 비밀번호입니다."},
+            )
 
 
 # 사용자 정보 수정
@@ -87,16 +96,16 @@ def pwd_change(request):
 def update(request):
     if request.method == "GET":
         user = User.objects.get(pk=request.user.pk)
-        form = 변경폼(instance=user)
-        return render(request, 'account/update.html', {'form':form})
+        form = CustomUserChangeForm(instance=user)
+        return render(request, "account/update.html", {"form": form})
     elif request.method == "POST":
         user = User.objects.get(pk=request.user.pk)
-        form = 변경폼(request.POST, request.FILES, instance=user)
+        form = CustomUserChangeForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect(reverse("account:detail"))
         else:
-            return render(request, 'account/update.html', {'form':form})
+            return render(request, "account/update.html", {"form": form})
 
 
 # 사용자 탈퇴
@@ -104,4 +113,4 @@ def update(request):
 def delete(request):
     request.user.delete()
     logout(request)
-    return redirect(reverse('HOME'))
+    return redirect(reverse("HOME"))
