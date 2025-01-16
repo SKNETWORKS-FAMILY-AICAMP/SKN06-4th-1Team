04890d15ce6b172b98f926a487845f824a523da5
Realoil <chungyuj33@gmail.com>
2025-01-16 17:33:38 +0900

    superuser 수정

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,23 @@
+from re import U
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ["username", "name", "state"]
+    # 사용자 추가
+    add_fieldsets = (
+        ("인증정보", {"fields": ("username", "password1", "password2")}),
+        ("개인정보", {"fields": ("name", "state", "birthday")}),
+        ("권한", {"fields": ("is_staff", "is_superuser")}),
+    )
+    # 사용자 변경
+    fieldsets = (
+        ("인증정보", {"fields": ("username", "password")}),
+        ("개인정보", {"fields": ("name", "state", "birthday")}),
+        ("권한", {"fields": ("is_staff", "is_superuser")}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
