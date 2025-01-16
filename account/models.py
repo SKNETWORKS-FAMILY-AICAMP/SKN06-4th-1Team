2a185613b7d0c51fb498ea3c2d72fdefbb70610c
Ko Sungju <sungjuko@gmail.com>
2025-01-16 16:48:17 +0900

    account/models, forms 추가가

--- account/models.py
+++ account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms import forms
 
-# Create your models here.
+# User 모델
+class User(AbstractUser):
+    # Field 정의 - table 컬럼
+    name = models.CharField(
+        verbose_name='이름',
+        max_length=50, # varcha(50)
+    )
+    state = models.CharField(
+        verbose_name='지역',
+        max_length=50, # varchar(50)
+    )
+    birthday = models.DateField(
+        verbose_name='생년월일',
+    )
+    profile_img = models.ImageField(
+        verbose_name='프로필 사진',
+        upload_to='images/%Y/%m/%d', # 저장경로
+        null=True,
+        blank=True, 
+    )
+
+    def __str__(self):
+        return f'username: {self.username}, name: {self.name}'
