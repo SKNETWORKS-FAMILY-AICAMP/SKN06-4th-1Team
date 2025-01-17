4ba15eea9bfd01049aaab6ae5a1f856418299b98
Realoil <chungyuj33@gmail.com>
2025-01-17 14:16:17 +0900

    poll/ admin에 question & answer 추가

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+# .models -> 상대 경로로 import. admin.py와 같은 패키지에 있는 models.py
+# admin app에서 데이터를 관리할 수 있도록 등록
+
+admin.site.register(Question)
+admin.site.register(Choice)
