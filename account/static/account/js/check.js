e7cfbb30c24ff7d5df01196b184e54b69cb41a3f
Realoil <chungyuj33@gmail.com>
2025-01-17 10:42:09 +0900

    account/ create 수정

--- account/static/account/js/check.js
+++ account/static/account/js/check.js
@@ -1,3 +1,12 @@
-function check_valid(name, age){
-    if (name.length < 2 && )
+function check_valid(name, birthday){
+    var this_year = new Date().getFullYear();
+    var age = this_year - birthday.substring(0,4);
+
+    if(age < 8 || age > 100){
+        return false;
+    }
+
+    if(name.length < 2){
+        return false;
+    }
 }
